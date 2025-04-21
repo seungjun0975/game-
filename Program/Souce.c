1c6b8eb0f76c95976222e3f2a187ab71ce0bcc32
seungjun0975 <207029129+seungjun0975@users.noreply.github.com>
2025-04-21 21:44:53 +0900

    (2025.04.21) 포인터

--- Program/Souce.c
+++ Program/Souce.c
@@ -1,39 +1,73 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-#pragma region 자료형 변환
-	// 서로 다른 자료형을 가지고 있는  변수끼리 연산이
-	// 이루어질 때 기존에지정했던 자료형을 다른 자료형
-	// 으로 변환하는 과정
+#pragma region 주소 연산자
+	// 변수의 주소 값을 반환하는 연산자입니다
 
-#pragma region 암묵적 자료형 변환
-	// 서로 다른 자료형으로 연산이 이루어질 때 자료형의
-	// 크기가 더 큰 자료형으로 변환되는 과정이다
-
-//	int integer = 10;
-//	float decimal = 5.5f;
+//int data = 10;
 //
-//	float storage = integer + decimal;
+//printf("데이터의 주소:%p\n",&data);
+
+	// 데이터의 주소는 해당 데이터가 저장된 메모리의 시작 주소를 
+	// 의미하며, 메모리의 공간은 1 byte로 크기로 나누어 표현합니다
+#pragma endregion
+	
+#pragma region scanf 함수
+	// 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에 
+	// 맞추어 입력해주는 함수입니다.
+
+//	int x = 0;
 //
-//	printf("storage 변수의 값: %f\n", storage);
-
-	// 표현 범위가 작은 데이터에 표현 범위가 큰 데이터를
-	// 저장하게 되면 암묵적으로 데이터 손실이 발생합니다
-#pragma endregion
-
-#pragma region 명시적 자료형 변환
-	// 연산이 이루어지기 전에 사용자가 직접 자료형을
-// 변환하는 과정
-//int critical = 10;
-//int penetration = 3;
-//float attack = critical / (float)penetration;
-//printf("attack:%f", attack);
-	// 정수형 변수끼리 연산을 수행하게되면 정수의 결과
-	// 값만 가질 수 있다 
-#pragma endregion
-
+//	printf("x변수의 값을 입력:");
+//
+//	// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로 
+//	// 넘어 갈수 없다 
+//
+//	scanf_s("%d", &x);//값X 주소넣어야함 ""안에 글자넣으면 안됨
+//
+//	// 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며,
+//	// 서식 지정자에 따라 입력할 수 잇는 데이터의 범위도 결정됩니다 
+//
+//	printf("x변수의 값: %d\n", x);
+//
+	// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에 
+	// 데이터를 보관하였다가 입력하는 순간 버퍼 안의 
+	// 내용을 프로그램에 전송합니다
 
 #pragma endregion
+
+#pragma region 포인터
+	// 메모리의 주소 값을 저장할 수 있는 변수
+
+	int storage = 10;     
+	int*pointer = &storage; 
+	
+	printf("%d\n", *pointer);
+	
+
+	// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로,
+	// 포인터가 가리키는 메모리의 자료형을 선언해주어야 합니다.
+
+	float* reference = &storage;
+
+	printf("storage의 주소는 : %p\n", &storage);
+	printf("pointer의 값: %p\n", pointer);
+	printf("pointer의 주소: %p\n", &pointer);
+
+	*pointer = 99;
+
+	printf("storage 변수의 값 : %d\n", storage);
+	printf("pointer 변수가 가리키는 값: %d\n", *pointer);
+
+	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
+	// 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수
+	// 의 시작 주소를 가리키게 됩니다.
+#pragma endregion
+
+	
+	
+	
+	
 	system("pause");
 }
